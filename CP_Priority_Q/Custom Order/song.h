a48b6b79bdaa1bf088b0972bc588b9cb5804d04d
Jacky007 <jojackchanah@gmail.com>
2024-10-14 14:51:44 +0700

    add Custom Order

--- CP_Priority_Q/Custom Order/song.h
+++ CP_Priority_Q/Custom Order/song.h
@@ -27,7 +27,33 @@ class Song {
 
 //  you have to write something below this line 
 //  you *MIGHT* have to change the declaration of pq1 and pq2
-CP::priority_queue<Song> pq1;
-CP::priority_queue<Song> pq2;
+struct Comp_pq1 {
+  bool operator()(Song &a, Song &b){
+    if(a.artist != b.artist){
+      return a.artist > b.artist;
+    }else if(a.artist == b.artist && a.title != b.title){
+      return a.title > b.title;
+    }else{
+      return a.count > b.count;
+    }
+
+  }
+};
+
+struct Comp_pq2 {
+  bool operator()(Song &a, Song &b){
+    if(a.count != b.count){
+      return a.count < b.count;
+    }else if(a.count == b.count && a.artist != b.artist){
+      return a.artist > b.artist;
+    }else{
+      return a.title > b.title;
+    }
+
+  }
+};
+
+CP::priority_queue<Song,Comp_pq1> pq1;
+CP::priority_queue<Song,Comp_pq2> pq2;
 
 #endif
