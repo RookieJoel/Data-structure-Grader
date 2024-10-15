49f254970ad58594f18c47398f3312e77619bd8b
Jacky007 <jojackchanah@gmail.com>
2024-10-15 22:15:50 +0700

    add Heap Check

--- CP_Priority_Q/Heap Check/student/student.h
+++ CP_Priority_Q/Heap Check/student/student.h
@@ -7,6 +7,13 @@ template <typename T, typename Comp>
 bool CP::priority_queue<T, Comp>::check() {
   // Your code here
   // Return something
+  int idx = 0,c;
+  while ((c = 2 * idx + 1) < mSize) {
+      T parent = mData[idx];
+      if (c < mSize && mLess(parent,mData[c])) return false; //check ลูกซ้าย
+      if (c + 1 < mSize && mLess(parent,mData[c+1])) return false; //check ลูกขวา
+      idx++;
+    }
   return true;
 }
 
