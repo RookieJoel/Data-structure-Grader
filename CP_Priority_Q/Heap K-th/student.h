b9f4b5d9e8f403970ccb6f7ad8d8db4fc15d9056
Jacky007 <jojackchanah@gmail.com>
2024-10-19 10:45:58 +0700

    add Heap K-th

--- CP_Priority_Q/Heap K-th/student.h
+++ CP_Priority_Q/Heap K-th/student.h
@@ -10,16 +10,12 @@ template <typename T,typename Comp >
 T CP::priority_queue<T,Comp>::get_kth(size_t k) const {
   //write your code here
   //can include anything
-  int idx = 0;
-  --k;
-  while(k--){
-    T temp = mData[idx];
-    int c1 = (2*idx)+1;
-    int c2 = (2*idx)+2;
-    if(mLess(temp,mData[c1])) idx = c1;
-    if(mLess(temp,mData[c2])) idx = c2;
-  }
-  return mData[idx];
+  if(k==1) return mData[0];
+  auto it = mData;
+  it+=k;
+  auto ans = std::max_element(it,mData+mSize);
+  return *ans;
+
 }
 
 #endif
