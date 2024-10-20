f50c34704deb50b7452ba2b338c7f10d3e3d391b
Jacky007 <jojackchanah@gmail.com>
2024-10-20 19:50:22 +0700

    add Vector Range Erase

--- CP_Vector/Vector Range Erase/student/student.h
+++ CP_Vector/Vector Range Erase/student/student.h
@@ -8,31 +8,34 @@
 template <typename T>
 void CP::vector<T>::range_erase(std::vector<std::pair<iterator, iterator>> &ranges) {
   // Write code here
+  std::sort(ranges.begin(),ranges.end());
   CP::vector<T> ans;
-  std::vector<std::pair<int,int>> v;
-  for(auto &x : ranges){
-    int a = x.first - begin();
-    int b = x.second - begin();
-    v.push_back({a,b});
+  std::vector<std::pair<int, int>> overlap;
+  int start = ranges[0].first - begin();
+  int end = ranges[0].second - begin();
+  for(int i = 1;i<ranges.size();i++){
+    if(ranges[i].first - begin() <= end){
+      end = std::max(end,ranges[i].second - begin());
+    }else{
+      overlap.push_back({start,end});
+      start = ranges[i].first - begin();
+      end = ranges[i].second - begin();
+    }
   }
+  overlap.push_back({start,end});
 
-  std::sort(v.begin(),v.end());
-    // for(auto &x : v) std::cout<< x.first << "," << x.second <<" ";
-
-  int p =0,j=0;
-  for(int i = 0;i<mSize;i++){
-    if(p< v.size() && i>=v[p].first && i < v[p].second){
-      if(i == v[p].second - 1) {
-        int temp = v[p].second;
-        while(v[p].first < temp){
-          p++;
-        }
-      }
+  // for(auto &x : overlap){
+  //   std::cout << "(" << x.first <<"," <<x.second << ") "; 
+  // }
+  int k = 0;
+  for(int i =0;i<mSize;i++){
+    if(k < overlap.size() && i >= overlap[k].first && i < overlap[k].second){
+      i = overlap[k].second -1;
+      k++;
     }else{
       ans.push_back(mData[i]);
     }
   }
-
   *this = ans;
 }
 
