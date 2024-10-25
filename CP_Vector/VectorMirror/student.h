d7847f671762adca7b6c093b6ec42a8f974a8861
Jacky007 <jojackchanah@gmail.com>
2024-10-25 19:29:07 +0700

    update Vector Mirror

--- CP_Vector/VectorMirror/student.h
+++ CP_Vector/VectorMirror/student.h
@@ -4,30 +4,12 @@
 template <typename T>
 void CP::vector<T>::mirror() {
   //write your code here
-  int cap = mSize*2;
-  T *arr = new T[cap]();
-  for(int i = 0;i<mSize;i++){
-    arr[i] = mData[i];
+  ensureCapacity(2*mSize);
+  int k=mSize-1;
+  for(int i=mSize;i<2*mSize;i++){
+    mData[i] = mData[k--];
   }
-
-  T *rev = new T[mSize]();
-  int j=0;
-  for(int i = mSize-1;i>=0;i--){
-    rev[j++] = mData[i];
-  }
-
-  // for(int i =0;i<mSize;i++) std::cout << rev[i] << " "<<std::endl;
-
-  int k = 0;
-  for(int i = mSize;i<2*mSize;i++){
-    arr[i] = rev[k++];
-  }
-
-  delete [] mData; 
-  delete [] rev;
-  mData = arr;
-  mCap = cap;
-  mSize = cap;
+  mSize*=2;
 }
 
 #endif
