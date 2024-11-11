a8b190426527fe38662ee4fce3edeee46b382f0b
Jacky007 <jojackchanah@gmail.com>
2024-11-11 10:13:29 +0700

    add SubTree Extract

--- CP_Map_BST/SubTree/student.h
+++ CP_Map_BST/SubTree/student.h
@@ -8,13 +8,31 @@
 template <typename KeyT,typename MappedT, typename CompareT >
 size_t CP::map_bst<KeyT,MappedT,CompareT>::process(node* n) {
   //write your code here
-  return 0;
+  if(n == NULL) return 0;
+  return process(n->left) + process(n->right) + 1;
 }
 
 template <typename KeyT,typename MappedT, typename CompareT >
 std::pair<KeyT,MappedT> CP::map_bst<KeyT,MappedT,CompareT>::subtree(map_bst<KeyT,MappedT,CompareT> &left, map_bst<KeyT,MappedT,CompareT> &right) {
   //write your code here
-  return std::pair<KeyT,MappedT>();
+  if(mSize == 0) return std::pair<KeyT,MappedT>();
+  auto fl = mRoot->left;
+  auto fr = mRoot->right;
+  left.clear();right.clear();
+  if(mRoot->left != nullptr){
+    left.mRoot = mRoot->left;
+    left.mRoot->parent = nullptr;
+  }if(mRoot->right != nullptr){
+    right.mRoot = mRoot->right;
+    right.mRoot->parent = nullptr;
+  } 
+  left.mSize = process(left.mRoot);
+  right.mSize = process(right.mRoot);
+
+  mRoot->left = nullptr;
+  mRoot->right = nullptr;
+  mSize = 1;
+  return mRoot->data;
 }
 
 #endif
